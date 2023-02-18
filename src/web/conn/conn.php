983c40174124328e111b425784e4adf043c41063
beckycheng <beckycheng63@gmail.com>
2023-02-18 17:52:48 +0800

    Delete all legacy code

--- src/web/conn/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_hostname = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "capstone_project";
-$conn = new mysqli($db_hostname, $db_username, $db_password, $db_name);
-$conn->set_charset("utf8mb4");
-
